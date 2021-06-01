362395b128897b6077db26343273842e1493e577
liufei <liufei1@codoon.com>
2021-06-01 14:15:22 +0800

    6月1日14：15

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/csv"
 	"errors"
 	"fmt"
 	"git.in.codoon.com/backend/common/clog"
+	"git.in.codoon.com/backend/common/thirdutil"
 	"git.in.codoon.com/backend/system_service/models"
 	"git.in.codoon.com/third/pinyin/pinyin"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/extensions"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type PublishRecord struct {
@@ -58,6 +65,72 @@ type PROVINCE_CITY_REGION_MODEL struct {
 	Area              string `gorm:"column:area" sql:"type:varchar(64)" json:"area"`
 }
 
+type ActiveMainSt struct {
+	Id             int64     `gorm:"primary_key;column:id"    sql:"type:int(11);auto_increment;not null" json:"id"`
+	CreateTime     time.Time `gorm:"column:create_time"       sql:"type:datetime;not null" json:"create_time"`
+	TpId           int       `gorm:"column:tp_id"             sql:"type:int(11);not null;default:0" json:"tp_id"`
+	StTime         time.Time `gorm:"column:st_time"           sql:"type:datetime" json:"st_time"`
+	EtTime         time.Time `gorm:"column:et_time"           sql:"type:datetime;not null" json:"et_time"`
+	Name           string    `gorm:"column:name"              sql:"type:varchar(25)" json:"name"`
+	Placard        string    `gorm:"column:placard"           sql:"type:varchar(200)" json:"placard"`
+	Details        string    `gorm:"column:details"           sql:"type:varchar(2000)" json:"details"`
+	Fee            int       `gorm:"column:fee"               sql:"type:int(11)" json:"fee"`
+	DeadLine       time.Time `gorm:"column:deadline"          sql:"type:datetime" json:"deadline"`
+	JoinNum        int       `gorm:"column:join_num"          sql:"type:int(11)" json:"join_num"`
+	ActualJoinNum  int       `gorm:"column:actual_join_num"   sql:"type:int(11)" json:"actual_join_num"`
+	IsNeedApproval int8      `gorm:"column:is_need_approval"  sql:"type:int(6)" json:"is_need_approval"`
+	IsNeedPhone    int8      `gorm:"column:is_need_phone"     sql:"type:int(6)" json:"is_need_phone"`
+	City           string    `gorm:"column:city"              sql:"type:varchar(20)" json:"city"`
+	IngAndLat      string    `gorm:"column:lng_and_lat"       sql:"type:varchar(50)" json:"lng_and_lat"`
+	Address        string    `gorm:"column:address"           sql:"type:varchar(50)" json:"address"`
+	CreateUserId   string    `gorm:"column:create_user_id"    sql:"type:varchar(36)" json:"create_user_id"`
+	IsCodoon       int8      `gorm:"column:is_codoon"         sql:"type:int(6)" json:"is_codoon"`
+	SortKey        int       `gorm:"column:sort_key"          sql:"type:int(11)" json:"sort_key"`
+	//0:正常 1:举报 2:删除 3:敏感 4取消
+	IsDelete        int8      `gorm:"column:is_delete"         sql:"type:int(6) json:"is_delete"`
+	FindCity        int       `gorm:"column:find_city"         sql:"type:int(11)" json:"find_city"`
+	CancelReason    string    `gorm:"column:cancel_reason"     sql:"type:varchar(300)" json:"cancel_reason"`
+	ThirdLink       string    `gorm:"column:third_link"        sql:"type:varchar(100)" json:"third_link"`
+	GroupId         int       `gorm:"column:group_id"          sql:"type:int(11)" json:"group_id"`
+	NeedGroupMember int       `gorm:"column:need_group_member" sql:"type:int(11)" json:"need_group_member"`
+	UpdateTime      time.Time `gorm:"column:update_time"       sql:"type:datetime" json:"update_time"`
+	DetailsUrl      string    `gorm:"column:details_url"       sql:"type:varchar(300)" json:"details_url"`
+	BrowseCount     int       `gorm:"column:browse_count"      sql:"type:int(11)" json:"browse_count"`
+	IsOfficePub     int8      `gorm:"column:is_office_pub"     sql:"type:int(6)" json:"is_office_pub"`
+	UserInfoFields  string    `gorm:"column:user_info_fields"  sql:"type:varchar(1024)" json:"user_info_fields"`
+	GpsSign         int8      `gorm:"column:gps_sign"          sql:"type:int(6)" json:"gps_sign"`
+	GpsSignTime     time.Time `gorm:"column:gps_sign_time"     sql:"type:datetime" json:"gps_sign_time"`
+	GpsIngAndLat    string    `gorm:"column:gps_lng_and_lat"   sql:"type:varchar(50)" json:"gps_lng_and_lat"`
+	//Phone           string    `gorm:"column:phone"             sql:"type:varchar(100)" json:"phone"`
+	PayType int `gorm:"column:pay_type"          sql:"type:int(11)" json:"pay_type"`
+	//是否支持退款 0不支持, 1支持
+	RefundType  int   `gorm:"column:refund_type"       sql:"type:int(11)" json:"refund_type"`
+	PayFee      int64 `gorm:"column:pay_fee"          sql:"type:bigint(20)" json:"pay_fee"`
+	ReportCount int   `gorm:"column:report_count"          sql:"type:int(11)" json:"report_count"`
+	FrozenType  int   `gorm:"column:frozen_type"          sql:"type:int(11)" json:"frozen_type"`
+	//1:基于位置签到 0 不受位置影响签到
+	IsSignWithLocation int8      `gorm:"column:is_sign_with_location"    sql:"type:int(6)" json:"is_sign_with_location"`
+	FromType           int8      `gorm:"column:from_type"         sql:"type:int(6)" json:"from_type"`
+	FromVersion        string    `gorm:"column:from_version"  sql:"type:varchar(16)" json:"from_version"`
+	DetailsRnModel     string    `gorm:"column:details_rn_model"           sql:"type:varchar(3000)" json:"details_rn_model"`
+	ActiveCityCode     string    `gorm:"column:active_city_code"           sql:"type:varchar(16)" json:"active_city_code"`
+	ActiveLoseTime     time.Time `gorm:"active_lose_time"                  sql:"type:datetime"    json:"active_lost_time"`
+
+	//新的报名规则字段
+	UserApplyRule     string `json:"user_apply_rule"`
+	ExtData           string `json:"ext_data"`
+	GpsLiveInfo       string `gorm:"column:gps_live_info"          sql:"type:varchar(1024)" json:"gps_live_info"`
+	EnableRanking     bool   `gorm:"not null;default:0" json:"enable_ranking"`      // 开启排名
+	ActivateUserCount int    `gorm:"not null;default:0" json:"activate_user_count"` // 总运动人数
+	TotalDistance     int    `gorm:"not null;default:0" json:"total_distance"`      // 总数据米
+
+}
+
+type CsvFile struct {
+	Name string
+	Data []byte
+}
+
 func main() {
 	//data, _ := GetProvinceUrlAndData("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/")
 	//fmt.Println(data)
@@ -72,9 +145,252 @@ func main() {
 
 	//data, _ := GetProvinceUrlAndData("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/")
 	//fmt.Println(data)
-	HasLetter("11")
+	//var name float64
+	//name = 81578
+	//accLength := name / 1000
+	//
+	//fmt.Println(float64(int(accLength*100)) / 100)
+
+	//f1, err := os.Open("a")
+	//if err != nil {
+	//
+	//}
+	//defer f1.Close()
+	//f2, err := os.Open("b")
+	//if err != nil {
+	//
+	//}
+	//defer f2.Close()
+	//f3, err := os.Open("c")
+	//if err != nil {
+	//
+	//}
+	//defer f3.Close()
+	//var files = []*os.File{f1, f2, f3}
+	//dest := "testCompress666.zip"
+	//err = Compress(files, dest)
+	//if err != nil {
+	//
+	//} else {
+	//	fmt.Println("压缩成功")
+	//}
+
+	//f1, _ :=os.Create("cs.csv")
+	//var files = []*os.File{f1}
+	//dest := "压缩cs.zip"
+	//err := Compress(files, dest)
+	//if err != nil {
+	//
+	//} else {
+	//	fmt.Println("压缩成功")
+	//}
+
+	//var aaa [][]string
+	//aaa =append(aaa,[]string{"111","111","222"})
+	//CreateCSV("aaa.csv",aaa)
 
 
+
+
+
+	//m3u8_dir_path := "13429235688@01@05@09"
+	//data := strings.Split(m3u8_dir_path, "@")
+	////需要在index=1的地方插入时间戳，方便以“id time emo val arou”的顺序写入csv文件
+	////将index=1后面的数据保存到一个临时的切片（其实是对index后面数据的复制）
+	//tmp := append([]string{}, data[1:]...)
+	//
+	////拼接插入的时间戳
+	//data = append(data[0:1], time.Now().Format("20060102150405"))
+	//
+	////与临时切片再组合得到最终的需要的切片
+	//data = append(data, tmp...)
+	//
+	//fmt.Printf("data[0]: %s\n", data[0])
+	//fmt.Printf("data[1]: %s\n", data[1])
+	//fmt.Printf("data[2]: %s\n", data[2])
+	//fmt.Printf("data[3]: %s\n", data[3])
+	//fmt.Printf("data[4]: %s\n", data[4])
+	//
+	//csvName := data[0] + ".csv"
+	//file, er := os.Open(csvName)
+	//defer file.Close()
+	//
+	//// 如果文件不存在，创建文件
+	//if er != nil && os.IsNotExist(er) {
+	//
+	//	file, er := os.Create(csvName)
+	//	if er != nil {
+	//		panic(er)
+	//	}
+	//	defer file.Close()
+	//
+	//	// 写入字段标题
+	//	w := csv.NewWriter(file) //创建一个新的写入文件流
+	//	title := []string{"user_id", "time", "emo", "val", "arou"}
+	//
+	//	// 这里必须刷新，才能将数据写入文件。
+	//	w.Write(title)
+	//	w.Write(data)
+	//	w.Flush()
+	//	fmt.Printf("if end")
+	//} else {
+	//	// 如果文件存在，直接加在末尾
+	//	txt, err := os.OpenFile(csvName, os.O_APPEND|os.O_RDWR, 0666)
+	//	defer txt.Close()
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	w := csv.NewWriter(txt) //创建一个新的写入文件流
+	//	w.Write(data)
+	//	w.Flush()
+	//	fmt.Printf("else end")
+	//}
+	//
+
+	//files := make([]CsvFile, 0)
+	//// 文件名， 每行的内容， 主题
+	//group := "csvfile1"
+	//
+	//var content [][]string
+	//content = append(content,[]string{"aaa","bbb","ccc"})
+	//content = append(content,[]string{"a2","22","c222"})
+	//
+	//title := []string{"第一列","第二列","第三列"}
+	//data, err := GenCSV(group, content, title)
+	//if err != nil {
+	//}
+	//file := CsvFile{
+	//	Name: fmt.Sprintf("%v.csv", group),
+	//	Data: data,
+	//}
+	//files = append(files, file)
+	//zipData, err := BytesZip(files)
+	//fileName := "测试文件1"
+	//
+	//fileUrl, err := PutZipToOss(zipData, fileName)
+	//fmt.Println(fileUrl)
+
+
+	title := []string{"1","2", "3"}
+	var rows [][]string
+	rows = append(rows,[]string{"a","b","c"})
+	rows = append([][]string{title}, rows...)
+	fmt.Println(rows)
+
+	generateCSV("csv111",rows)
+
+
+
+}
+
+func PutZipToOss(data []byte, fileName string) (string, error) {
+	filUrl, err := thirdutil.DefaultOssClient.PutU(fileName, data)
+	return filUrl, err
+}
+
+
+func BytesZip(files []CsvFile) ([]byte, error) {
+	buff := new(bytes.Buffer)
+	w := zip.NewWriter(buff)
+	for _, v := range files {
+		ww, err := w.Create(v.Name)
+		if err != nil {
+			return nil, err
+		}
+		_, err = ww.Write(v.Data)
+		if err != nil {
+			return nil, err
+		}
+	}
+	err := w.Close()
+	if err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
+}
+
+func GenCSV(groupName string, rows [][]string, title []string) ([]byte, error) {
+	if groupName == "" {
+		return nil, errors.New("sheet name is nil")
+	}
+	if title != nil && len(title) > 0 {
+		rows = append([][]string{title}, rows...)
+	}
+	buf := bytes.NewBuffer(nil)
+	w := csv.NewWriter(buf)
+	err := w.Write([]string{"\xEF\xBB\xBF"})
+	if err != nil {
+		return nil, err
+	}
+	err = w.WriteAll(rows)
+	return buf.Bytes(), err
+}
+
+func generateCSV(csvName string, rows [][]string) ([]byte, error) {
+
+	if csvName == "" {
+		return nil, errors.New("sheet name is nil")
+	}
+	buf := bytes.NewBuffer(nil)
+	w := csv.NewWriter(buf)
+	err := w.Write([]string{"\xEF\xBB\xBF"})
+	if err != nil {
+		return nil, err
+	}
+	err = w.WriteAll(rows)
+	return buf.Bytes(), err
+}
+
+//生成csv文件
+func CreateCSV(txtname string,title [][]string)  {
+	f , err := os.Create(txtname)
+	if err != nil{
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("\xEF\xBB\xBF")
+	w:=csv.NewWriter(f)
+	w.WriteAll(title)
+	w.Flush()
+}
+
+//压缩文件
+//files 文件数组，可以是不同dir下的文件或者文件夹
+//dest 压缩文件存放地址
+func Compress(files []*os.File, dest string) error {
+	d, _ := os.Create(dest)
+	defer d.Close()
+	w := zip.NewWriter(d)
+	defer w.Close()
+	for _, file := range files {
+		err := compress(file, "", w)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func compress(file *os.File, prefix string, zw *zip.Writer) error {
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+	header.Name = prefix + "/" + header.Name
+	writer, err := zw.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, file)
+	file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func HasLetter(s string) {
